2a8e2f1e842e62eff8a15559c341c7ee2945194c
Amit <raimeghna25@gmail.com>
2026-04-20 21:12:28 +0530

    first js program

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("This is my first javascript program")
+console.log("This is my second javascript program")
+console.log("This is my third javascript program")
+
+let emailaddress = "myemail@gmail.com";
+const name ="Meghna rai";
+var middleName ="M";
+console.log(emailaddress);
+console.log(name);
+console.log(middleName);
+emailaddress = "newemail@gmail.com";
+//name = "Trayaksh rai";
+middleName = "K";
+console.log(emailaddress);
+console.log(name);
+console.log(middleName);
